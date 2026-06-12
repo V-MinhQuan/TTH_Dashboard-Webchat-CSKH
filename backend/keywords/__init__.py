77343502f8db1e4aeb62c4cbe279efe4817f7d6b
Wiam <quantk11123@gmail.com>
2026-06-12 14:19:04 +0700

    Remove all files from main branch

--- backend/keywords/__init__.py
+++
@@ -1,5 +0,0 @@
-# Keywords feature module
-from backend.keywords.repository import keyword_repository
-from backend.keywords.service import keyword_service
-
-__all__ = ["keyword_repository", "keyword_service"]
